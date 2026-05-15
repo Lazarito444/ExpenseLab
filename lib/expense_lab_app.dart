4c101eccf79793ab8d46ea02d372f1e92265f595
Ariel Lázaro <ariellazaro444@gmail.com>
2026-05-15 09:09:29 -0400

    cleanup main.dart

---
+++ lib/expense_lab_app.dart
@@ -0,0 +1,28 @@
+import 'package:expenselab/core/providers/settings_providers.dart';
+import 'package:expenselab/core/theme/app_theme.dart';
+import 'package:expenselab/i18n/strings.g.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ExpenseLabApp extends ConsumerWidget {
+  const ExpenseLabApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+    return MaterialApp(
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: themeMode,
+      locale: TranslationProvider.of(context).flutterLocale,
+      supportedLocales: AppLocaleUtils.supportedLocales,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: const Placeholder(),
+    );
+  }
+}
