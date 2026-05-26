687366cde1e40c356dc0ff87f6b31b62d45fbfca
Ariel Lázaro <ariellazaro444@gmail.com>
2026-05-25 20:22:48 -0400

    finish app bar

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
 
-  static const Color seedColor = Color(0xFF2E7D32);
+  static const Color seedColor = Color(0xFF2D6831);
 
   static ThemeData get light => ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: seedColor,
       brightness: Brightness.light,
-      primary: const Color(0xFF2D6831),
+      seedColor: seedColor,
+      primary: seedColor,
       primaryFixed: const Color(0xFF4E8A4F),
     ),
     textTheme: const TextTheme(
@@ -19,38 +19,38 @@ class AppTheme {
         fontFamily: 'Epilogue',
         fontWeight: FontWeight.w600,
         color: Color(0xFF2D6831),
-        fontSize: 24,
+        fontSize: 20,
       ),
-      headlineLarge: TextStyle(
-        fontFamily: 'Epilogue',
-        fontWeight: FontWeight.w600,
-        color: Colors.black,
-        fontSize: 30,
-      ),
-      headlineSmall: TextStyle(
-        fontFamily: 'Epilogue',
-        fontWeight: FontWeight.w400,
-        color: Color(0xFF474f45),
-        fontSize: 15,
-      ),
-      labelSmall: TextStyle(
-        fontFamily: 'Epilogue',
-        fontWeight: FontWeight.w500,
-        color: Color(0xFFD3E6CF),
-        fontSize: 16,
-      ),
-      displayMedium: TextStyle(
-        fontFamily: 'Epilogue',
-        fontWeight: FontWeight.w600,
-        color: Colors.white,
-        fontSize: 34,
-      ),
-      labelMedium: TextStyle(
-        fontFamily: 'Epilogue',
-        fontWeight: FontWeight.w700,
-        color: Color(0xFFD3E6CF),
-        fontSize: 15,
-      ),
+      // headlineLarge: TextStyle(
+      //   fontFamily: 'Epilogue',
+      //   fontWeight: FontWeight.w600,
+      //   color: Colors.black,
+      //   fontSize: 30,
+      // ),
+      // headlineSmall: TextStyle(
+      //   fontFamily: 'Epilogue',
+      //   fontWeight: FontWeight.w400,
+      //   color: Color(0xFF474f45),
+      //   fontSize: 15,
+      // ),
+      // labelSmall: TextStyle(
+      //   fontFamily: 'Epilogue',
+      //   fontWeight: FontWeight.w500,
+      //   color: Color(0xFFD3E6CF),
+      //   fontSize: 16,
+      // ),
+      // displayMedium: TextStyle(
+      //   fontFamily: 'Epilogue',
+      //   fontWeight: FontWeight.w600,
+      //   color: Colors.white,
+      //   fontSize: 34,
+      // ),
+      // labelMedium: TextStyle(
+      //   fontFamily: 'Epilogue',
+      //   fontWeight: FontWeight.w700,
+      //   color: Color(0xFFD3E6CF),
+      //   fontSize: 15,
+      // ),
     ),
     scaffoldBackgroundColor: const Color(0xFFF8F9F8),
   );
--- lib/widgets/expense_lab_app_bar.dart
+++ lib/widgets/expense_lab_app_bar.dart
@@ -1,14 +1,16 @@
+import 'package:expenselab/core/extensions/context_extensions.dart';
+import 'package:expenselab/core/i18n/strings.g.dart';
 import 'package:expenselab/features/settings/providers/settings_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ExpenseLabAppBar extends ConsumerWidget implements PreferredSizeWidget {
-  final String title;
+  final String? title;
   final Widget? leading;
   final List<Widget>? actions;
 
   const ExpenseLabAppBar({
-    required this.title,
+    this.title,
     this.leading,
     this.actions,
     super.key,
@@ -18,19 +20,29 @@ class ExpenseLabAppBar extends ConsumerWidget implements PreferredSizeWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeModeProvider);
     final isDark = themeMode == ThemeMode.dark;
+    final t = context.t;
+
     return AppBar(
       elevation: 0,
       backgroundColor: isDark ? const Color(0xFF1E2420) : Colors.white,
-      leading: leading,
-      actions: actions,
-      title: Text(
-        title,
-        style: const TextStyle(
-          fontFamily: 'Epilogue',
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 12),
+        child: Center(
+          child: SizedBox(
+            height: 42,
+            width: 42,
+            child:
+                leading ??
+                const CircleAvatar(
+                  child: Icon(
+                    Icons.account_balance_outlined,
+                  ),
+                ),
+          ),
         ),
       ),
+      actions: actions,
+      title: Text(title ?? t.app.name, style: context.textTheme.titleMedium),
     );
   }
 
