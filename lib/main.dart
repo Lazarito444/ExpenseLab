e8b82ea152e161fbe3a22695cf620f9471d128c7
Ariel Lázaro <ariellazaro444@gmail.com>
2026-06-04 13:08:31 -0400

    init app with preferred lang

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,20 @@ import 'package:expenselab/core/i18n/strings.g.dart';
 import 'package:expenselab/expense_lab_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   launchExpenseLabApp();
 }
 
-void launchExpenseLabApp() {
+Future<void> launchExpenseLabApp() async {
   WidgetsFlutterBinding.ensureInitialized();
-  LocaleSettings.useDeviceLocale();
+  final prefs = await SharedPreferences.getInstance();
+  final savedLocale = prefs.getString('settings_locale');
+  final locale = savedLocale != null
+      ? AppLocaleUtils.parse(savedLocale)
+      : AppLocaleUtils.findDeviceLocale();
+  LocaleSettings.setLocale(locale);
   runApp(
     ProviderScope(
       child: TranslationProvider(
